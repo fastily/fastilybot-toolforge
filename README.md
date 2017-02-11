ada56c08eadee2688061e7b2ad62abd6d231ca6b
fastily <fastily@users.noreply.github.com>
2017-02-10 18:48:45 -0800

    initial commit, add fastilybot at wmflabs files

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # wmf-toollabs
+
+Utilities and database reports hosted out of WMF labs.
+
+* [fastilybot](https://tools.wmflabs.org/fastilybot)
+
+### See Also
+* [ctools](https://github.com/fastily/ctools)
---
+++ fastilybot/scripts/findFilesOnWPandCOM.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#: Creates a report containing all en.wikipedia files with a duplicate on Commons.
+#:
+#: Author: Fastily
+
+cd "${0%/*}" &> /dev/null
+
+OUTFOLDER="../public_html/reports"
+OUTFILE="${OUTFOLDER}/wpDupes.txt"
+
+mkdir -p "$OUTFOLDER"
+
+mysql --defaults-file="${HOME}"/replica.my.cnf -h enwiki.labsdb enwiki_p -B < findFilesOnWPandCOM.sql > "$OUTFILE"
+
+## First two lines are junk
+sed -i -e '1,3d' "$OUTFILE"
