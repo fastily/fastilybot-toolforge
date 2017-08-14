b460289ed76db82d7e7c1a18565119b041eb8a09
fastily <fastily@users.noreply.github.com>
2017-08-14 02:38:07 -0700

    removed extraneous navbar code, cleanup

--- public_html/index.php
+++ public_html/index.php
@@ -12,30 +12,14 @@
 	<link rel="stylesheet" href="index.css" >	
 </head>
 <body>
-	<nav class="navbar navbar-default navbar-fixed-top">
-		<div class="container">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="#myPage">fastilybot</a>
-			</div>
-			<div class="collapse navbar-collapse" id="myNavbar">
-				<ul class="nav navbar-nav navbar-right">
-					<li>
-						<a href="https://en.wikipedia.org/wiki/User:FastilyBot">BOT</a>
-					</li>
-					<li>
-						<a href="https://en.wikipedia.org/wiki/User:Fastily">AUTHOR</a>
-					</li>
-					<li>
-						<a href="https://github.com/fastily/wmf-toollabs">SOURCE</a>
-					</li>
-				</ul>
-			</div>
-		</div>
+	<nav class="navbar navbar-default">
+	  <div class="container-fluid">
+	    <ul class="nav navbar-nav navbar-right">
+			<li><a href="https://en.wikipedia.org/wiki/User:FastilyBot">BOT</a></li>
+			<li><a href="https://en.wikipedia.org/wiki/User:Fastily">AUTHOR</a></li>
+			<li><a href="https://github.com/fastily/wmf-toollabs">SOURCE</a></li>
+	    </ul>
+	  </div>
 	</nav>
 
 	<div class="jumbotron text-center">
@@ -68,7 +52,7 @@
 
 	<footer class="container-fluid text-center footer">
 		<p>
-			Powered by <a href="https://tools.wmflabs.org">WMF toollabs</a> &mdash; &copy; 2017, Fastily, <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">GPLv3</a>
+			Powered by <a href="https://tools.wmflabs.org">WMF toolforge</a> &mdash; &copy; 2017, Fastily, <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">GPLv3</a>
 		</p>
 	</footer>
 
