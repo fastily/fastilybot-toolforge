bd952d86614e326d8c7ddc0823f7b042f1af9ab2
fastily <fastily@users.noreply.github.com>
2017-09-18 00:05:10 -0700

    fix report8 - should only select actual files

--- scripts/report8.sql
+++ scripts/report8.sql
@@ -2,6 +2,5 @@
  * List of all pages in the 'File:' namespace on enwp.  Cached list -> no clobbering the server.
  * Author: Fastily
  */
-SELECT pg.page_title 
-FROM page pg
-WHERE pg.page_namespace=6 AND pg.page_is_redirect=0;
+SELECT image.img_name
+FROM image;
