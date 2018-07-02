a3cb7da1030dcb2f47aae4541a08dd4c02a24288
fastily <fastily@users.noreply.github.com>
2018-07-02 00:27:40 -0700

    modified runReport to accept multuple args

--- public_html/index.php
+++ public_html/index.php
@@ -47,8 +47,8 @@
 					generateRow(6, "all free files on enwp", "Daily");
 					generateRow(7, "oversized non-free images on enwp", "Weekly");
 					generateRow(8, "all files on enwp", "Daily");
-					generateRow(9, "orphaned free files on enwp", "Daily");
-					generateRow(10, "orphaned, low-res free files on enwp", "Daily");
+					generateRow(9, "orphaned free files on enwp", "Weekly");
+					generateRow(10, "orphaned, low-res free files on enwp", "Weekly");
 				?>
 			</tbody>
 		</table>
--- scripts/runReport.sh
+++ scripts/runReport.sh
@@ -3,14 +3,19 @@
 #: Runs a MySQL query against the labs replica database and puts the result in ~/public_html/r.
 #:
 #:	Arguments:
-#:		1) The integer id of the report to run.
+#:		1) The integer id(s) of the report to run.
 #:
 #: Author: Fastily
 
+if [ $# -lt 1 ]; then
+	printf "Usage: runReport.sh <REPORT_NUMBERS...>\n"
+	exit
+fi
+
 cd ~/scripts
-reportFile=~/"public_html/r/report${1}.txt"
 
-mysql --defaults-file=~/"replica.my.cnf" -h enwiki.labsdb enwiki_p -B < ~/"scripts/report${1}.sql" > "$reportFile"
-
-## First two lines are junk
-sed -i -e '1,3d' "$reportFile"
+for i in "$@"; do
+	reportFile=~/"public_html/r/report${i}.txt"
+	mysql --defaults-file=~/"replica.my.cnf" -h enwiki.labsdb enwiki_p -B < ~/"scripts/report${i}.sql" > "$reportFile"
+	sed -i -e '1,3d' "$reportFile" 	# First two lines are junk
+done
