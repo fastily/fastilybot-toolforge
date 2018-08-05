78c3649c5ac317b23b68e85f15486877a0b4e7c7
fastily <fastily@users.noreply.github.com>
2018-08-04 23:18:28 -0700

    tweak report10 width/height

--- scripts/report10.sql
+++ scripts/report10.sql
@@ -10,5 +10,5 @@ ON img.img_name=pg.page_title
 WHERE  cl.cl_to='Wikipedia_orphaned_files'
 AND pg.page_namespace=6
 AND img.img_media_type='BITMAP'
-AND img.img_width<500
-AND img.img_height<500;
+AND img.img_width<300
+AND img.img_height<300;
