5c7448069f9ee96053096d5837ec013ee5454829
fastily <fastily@users.noreply.github.com>
2019-08-21 23:31:22 -0700

    updated report10 to filter prod files

--- scripts/report10.sql
+++ scripts/report10.sql
@@ -2,13 +2,20 @@
  * Lists low-res, orphaned free images on enwp.
  * Author: Fastily
  */
-SELECT pg.page_title FROM categorylinks cl
-INNER JOIN page pg
-ON cl.cl_from=pg.page_id
-INNER JOIN image img
-ON img.img_name=pg.page_title
-WHERE cl.cl_to='Wikipedia_orphaned_files'
-AND img.img_media_type='BITMAP'
-AND img.img_width<250
-AND img.img_height<250
-LIMIT 1250;
+ SELECT pg.page_title FROM categorylinks cl
+ INNER JOIN page pg
+ ON cl.cl_from=pg.page_id
+ INNER JOIN image img
+ ON img.img_name=pg.page_title
+ WHERE cl.cl_to='Wikipedia_orphaned_files'
+ AND img.img_media_type='BITMAP'
+ AND img.img_width<250
+ AND img.img_height<250
+ AND pg.page_title NOT IN (
+ 	SELECT p.page_title FROM categorylinks c
+ 	INNER JOIN page p
+ 	ON c.cl_from=p.page_id
+ 	WHERE c.cl_to='All_files_proposed_for_deletion'
+ 	AND p.page_namespace=6
+ )
+ LIMIT 1500
