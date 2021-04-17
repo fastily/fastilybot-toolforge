8dc8ca1495c55d1df78fe8c3332b28b19f8cc7c8
rubin16 <rubin16@users.noreply.github.com>
2021-04-17 18:49:08 +0300

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -35,7 +35,7 @@
 
 	<link rel="stylesheet" href="https://tools-static.wmflabs.org/cdnjs/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css" crossorigin="anonymous">
 
-	<title>fastilybot @ toolforge</title>
+	<title>fastilybot @ toolforge (forked for ru.wiki by rubin16)</title>
 </head>
 
 <body>
@@ -74,23 +74,23 @@
 			</thead>
 			<tbody>
 				<?php
-					generateRow(1, "enwp files with a duplicate on Commons", "Tri-weekly"); 
-					generateRow(2, "enwp files tagged as free and non-free", "Weekly"); 
-					generateRow(3, "free files on enwp with no fileusage", "Weekly");
-					generateRow(4, "free files on enwp with links to the mainspace", "Weekly");
-					generateRow(5, "all non-free files on enwp", "Weekly");
-					generateRow(6, "all free files on enwp", "Weekly");
-					generateRow(7, "oversized non-free images on enwp", "Weekly");
-					generateRow(8, "all files on enwp", "Weekly");
-					generateRow(9, "orphaned free files on enwp", "Weekly");
-					generateRow(10, "orphaned, low-res free files on enwp", "Weekly");
-					generateRow(11, "enwp file pages shadowing commons files", "Weekly");
-					generateRow(12, "enwp files without an obvious/machine-readable source", "Weekly");
-					generateRow(13, "enwp files shadowing a Commons file", "Weekly");
-					generateRow(14, "transcluded non-existent templates on enwp", "Weekly");
-					generateRow(15, "non-free pdfs on enwp", "Weekly");
-					generateRow(16, "orphaned file talk pages on enwp", "Weekly");
-					generateRow(17, "all spi reports on enwp", "Weekly");
+					generateRow(1, "files with a duplicate on Commons", "Tri-weekly"); 
+#					generateRow(2, "enwp files tagged as free and non-free", "Weekly"); 
+#					generateRow(3, "free files on enwp with no fileusage", "Weekly");
+#					generateRow(4, "free files on enwp with links to the mainspace", "Weekly");
+#					generateRow(5, "all non-free files on enwp", "Weekly");
+#					generateRow(6, "all free files on enwp", "Weekly");
+#					generateRow(7, "oversized non-free images on enwp", "Weekly");
+#					generateRow(8, "all files on enwp", "Weekly");
+#					generateRow(9, "orphaned free files on enwp", "Weekly");
+					generateRow(10, "orphaned, low-res free files", "Weekly");
+					generateRow(11, "file pages shadowing commons files", "Weekly");
+#					generateRow(12, "enwp files without an obvious/machine-readable source", "Weekly");
+					generateRow(13, "files shadowing a Commons file", "Weekly");
+#					generateRow(14, "transcluded non-existent templates on enwp", "Weekly");
+#					generateRow(15, "non-free pdfs on enwp", "Weekly");
+					generateRow(16, "orphaned file talk pages ", "Weekly");
+#					generateRow(17, "all spi reports on enwp", "Weekly");
 				?>
 			</tbody>
 		</table>
