e766a89765ebfe1249dce76480e7c46a4a8eebe6
fastily <fastily@users.noreply.github.com>
2022-01-12 01:16:31 -0800

    bug fix - ignore case in r22

--- scripts/report22.sql
+++ scripts/report22.sql
@@ -4,4 +4,4 @@
  */
 SELECT ii.img_name
 FROM image ii
-WHERE ii.img_name REGEXP ".*?(\\.(djvu|flac|gif|jpeg|jpg|mid|midi|mp3|mpeg|mpg|oga|ogg|ogv|opus|pdf|png|svg|tif|tiff|wav|webm|webp|xcf)){2,}";
+WHERE ii.img_name REGEXP "(?i).*?(\\.(djvu|flac|gif|jpeg|jpg|mid|midi|mp3|mpeg|mpg|oga|ogg|ogv|opus|pdf|png|svg|tif|tiff|wav|webm|webp|xcf)){2,}";
