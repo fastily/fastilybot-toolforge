d87be960f06f930346634d8518eaf39017657b7d
fastily <fastily@users.noreply.github.com>
2022-09-15 02:07:06 -0700

    update scripts per changes to templatelinks table
    credit for report14 changes goes to Cryptic at enwp

--- scripts/report12.sql
+++ scripts/report12.sql
@@ -13,8 +13,13 @@ ON cl.cl_from=pg.page_id AND cl.cl_to='Files_with_no_machine-readable_source'
 LEFT JOIN categorylinks cnot
 ON cnot.cl_from=pg.page_id AND cnot.cl_to IN ('All_non-free_media', 'Wikipedia_files_tagged_as_own_work', 'Self-published_work', 'User-created_public_domain_files')
 
-LEFT JOIN templatelinks tnot
-ON tnot.tl_from=pg.page_id AND tnot.tl_title IN ('Information', 'Keep_local')
+LEFT JOIN (
+    SELECT t.tl_from, l.lt_title
+    FROM templatelinks t
+    INNER JOIN linktarget l
+    ON t.tl_target_id = l.lt_id
+) tnot
+ON tnot.tl_from=pg.page_id AND tnot.lt_title IN ('Information', 'Keep_local')
 
 WHERE cnot.cl_from IS NULL AND tnot.tl_from IS NULL
 LIMIT 1000;
--- scripts/report14.sql
+++ scripts/report14.sql
@@ -2,11 +2,13 @@
  * Lists non-existent templates that are transcluded in places besides user scripts.
  * Author: Fastily
  */
-SELECT t.tl_title FROM templatelinks t 
-LEFT JOIN page p 
-ON p.page_namespace = t.tl_namespace AND p.page_title = t.tl_title 
+SELECT t.tl_title FROM templatelinks t
+INNER JOIN linktarget lt
+ON lt.lt_id = t.tl_target_id
+LEFT JOIN page p
+ON p.page_namespace = lt.lt_namespace AND p.page_title = lt.lt_title
 INNER JOIN page p2
 ON t.tl_from  = p2.page_id
-WHERE p.page_title IS NULL AND t.tl_namespace = 10 AND p2.page_content_model="wikitext"
+WHERE p.page_title IS NULL AND lt.lt_namespace = 10 AND p2.page_content_model="wikitext"
 GROUP BY 1
 LIMIT 200;
